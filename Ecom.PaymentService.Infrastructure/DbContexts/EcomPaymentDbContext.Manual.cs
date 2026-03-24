05e2d9510c3206c041a0279a1ac587b5d2da61f7
nguyenthinh28902 <nndthinh28902@gmail.com>
2026-03-24 14:28:53 +0700

    fix build entity

---
+++ Ecom.PaymentService.Infrastructure/DbContexts/EcomPaymentDbContext.Manual.cs
@@ -0,0 +1,20 @@
+﻿using Ecom.PaymentService.Core.Models.Connection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecom.PaymentService.Infrastructure.DbContexts
+{
+    public partial class EcomPaymentDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Đưa logic dùng biến hằng của bạn vào đây
+                optionsBuilder.UseSqlServer(ConnectionStrings.EcomPaymentDb);
+            }
+        }
+    }
+}
--- Ecom.PaymentService.Infrastructure/DbContexts/EcomPaymentDbContext.cs
+++ Ecom.PaymentService.Infrastructure/DbContexts/EcomPaymentDbContext.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using Ecom.PaymentService.Core.Entities;
+﻿using Ecom.PaymentService.Core.Entities;
+using Ecom.PaymentService.Core.Models.Connection;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 
 namespace Ecom.PaymentService.Infrastructure.DbContexts;
 
@@ -21,10 +22,6 @@ public partial class EcomPaymentDbContext : DbContext
     public virtual DbSet<Transaction> Transactions { get; set; }
 
     public virtual DbSet<TransactionLog> TransactionLogs { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=Ecom_PaymentDb;Persist Security Info=False;User ID=demo;Password=Thinh@zzxx9;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
